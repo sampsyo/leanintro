432089abb034aa3c6abf472d645cce6d99f35e0a
Adrian Sampson <adrian@radbox.org>
2016-12-23 16:12:41 -0500

    More robust initial window open

--- sendcode.js
+++ sendcode.js
@@ -1,20 +1,24 @@
-const LEAN_URL = 'lean/?live';
+var LEAN_URL = 'lean/?live';
 
 document.addEventListener("DOMContentLoaded", function () {
-  let the_window;
+  var the_window;
   function register_example(example) {
     example.addEventListener('click', function () {
-      let code = example.textContent.trim();
+      var code = example.textContent.trim();
 
-      if (!the_window || !the_window.opener || the_window.closed) {
+      if (the_window && the_window.opener && !the_window.closed) {
+        the_window.postMessage({"code": code}, window.location.origin);
+      } else {
+        var url = LEAN_URL + '&code=' +
+          btoa(unescape(encodeURIComponent(code)));
         the_window = window.open(LEAN_URL, '_dingus');
+        console.log(the_window);
       }
-      the_window.postMessage({"code": code}, window.location.origin);
     });
   }
 
-  let examples = document.querySelectorAll('.example');
-  for (let i = 0; i < examples.length; ++i) {
+  var examples = document.querySelectorAll('.example');
+  for (var i = 0; i < examples.length; ++i) {
     register_example(examples[i]);
   }
 });
